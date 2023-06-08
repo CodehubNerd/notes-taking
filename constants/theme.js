3b13079988b107e7df18c777929a6302e010e98b
codehubNerd <dlaminisaneliso75@gmail.com>
2023-06-08 15:17:09 +0200

    file structure setup

---
+++ constants/theme.js
@@ -0,0 +1,19 @@
+const COlORS = {
+    primary: "#010101",
+    secondary: "F46A4E",
+    tertiary:"#3A3A3A",
+    gray: "#989898",
+    white:"#FAFAFA"
+};
+
+const FONT = {
+    ExtraBold: "ExtraBold",
+    semibold: "semibold",
+    medium: "medium",
+};
+
+const SIZES = {
+    medium: 11,
+    small: 10,
+    large: 30
+};
