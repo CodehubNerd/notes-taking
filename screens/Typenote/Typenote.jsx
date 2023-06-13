0ca5b72bbbd0cbfb486bbae42c341f6b4ca9ddaf
codehubNerd <dlaminisaneliso75@gmail.com>
2023-06-13 23:48:21 +0200

    added react hooks

--- screens/Typenote/Typenote.jsx
+++ screens/Typenote/Typenote.jsx
@@ -1,16 +1,18 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View,Text,TextInput} from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import styles from "./typenote.style";
 
 
 const Typenote = () => {
+  const [text, seText] = useState('');
+  //pop or focus on the input feild 
   return (
     <View style ={styles.container}>
       <View style = {styles.iconscontainer}>
       <AntDesign name="back" size={24} color="white" />
       </View>
-      
+
     </View>
   )
 }
