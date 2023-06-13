42c5640d6af1f74b8024efa9e4b682fc1056cc6e
codehubNerd <dlaminisaneliso75@gmail.com>
2023-06-13 23:56:01 +0200

    error in type note screen

--- screens/Typenote/Typenote.jsx
+++ screens/Typenote/Typenote.jsx
@@ -15,13 +15,23 @@ const Typenote = () => {
   const handleTextChange = (newText) => {
     setText(newText);
   };
-  
+
   let inputRef;
   return (
     <View style ={styles.container}>
       <View style = {styles.iconscontainer}>
       <AntDesign name="back" size={24} color="white" />
       </View>
+      
+      <TextInput
+        style={styles.input}
+        value={text}
+        onChangeText={handleTextChange}
+        placeholder="Type something..."
+        ref={(ref) => { inputRef = ref; }}
+      />
+      
+      <Text>{text}</Text>
 
     </View>
   )
