ddc360afeb50371741220802dd143eb7640cb960
codehubNerd <dlaminisaneliso75@gmail.com>
2023-06-13 23:22:16 +0200

    importing styles

--- screens/Typenote/Typenote.jsx
+++ screens/Typenote/Typenote.jsx
@@ -1,6 +1,8 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { AntDesign } from '@expo/vector-icons';
+import styles from "./home.style";
+
 
 const Typenote = () => {
   return (
